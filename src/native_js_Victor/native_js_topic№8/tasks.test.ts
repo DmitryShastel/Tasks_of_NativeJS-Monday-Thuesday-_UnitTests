e7068fbbf58759ffeb3b9407c29944e72add7ff5
DmitryShastel <dmshastel@gmail.com>
2021-03-25 20:38:42 +0300

    n_js№8_tasks_unit_tests_2,3tasks

--- src/native_js_Victor/native_js_topic№8/tasks.test.ts
+++ src/native_js_Victor/native_js_topic№8/tasks.test.ts
@@ -1,5 +1,4 @@
-import {sum} from "../native_js_topic№7_unit_tests/functions_for_students";
-import {summ} from "./tasks";
+import {getSum, getTriangleType, summ} from "./tasks";
 
 test('summa function summ should be correct', () =>{
     const a = 3
@@ -7,4 +6,18 @@ test('summa function summ should be correct', () =>{
     const result = summ(a,b)
     expect(result).toBe(8)
 } )
+test("get Triangle Type", ()=> {
+    expect(getTriangleType(1, 1, 1)).toBe("10")
+    expect(getTriangleType(3, 3, 2)).toBe("01")
+    expect(getTriangleType(5, 4, 3)).toBe("11")
+    expect(getTriangleType(10, 5, 5)).toBe("00")
+})
+
+test("get Sum ", ()=> {
+    expect(getSum(1000)).toBe(1)
+    expect(getSum(0)).toBe(0)
+    expect(getSum(1234)).toBe(10)
+    expect(getSum(9999)).toBe(36)
+})
+
 
--- src/native_js_Victor/native_js_topic№8/tasks.ts
+++ src/native_js_Victor/native_js_topic№8/tasks.ts
@@ -4,5 +4,24 @@ export let summ = (a: number,b: number) =>{
 }
 
 //2
+export let getTriangleType = (a: number,b: number,c: number)=>  {
+   if(a+b>c && b+c>a && c+a>b) {
+       if(a == b && b == c && c ==a){
+           return "10"
+       }else if (a === b || b === c || c === a) {
+           return "01"
+       }else {
+           return "11"
+       }
+   } else  {
+        return "00"
+    }
+}
 
-export default 11;
+//3
+export function getSum(number: number) {
+    return number.toString().split('').reduce((acc, el  ) => acc + Number(el),0)
+
+
+}
+
