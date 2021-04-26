1e805aa4816afc26dfe6ac5b9fdb55005dbef0f3
DmitryShastel <dmshastel@gmail.com>
2021-04-27 00:35:48 +0300

    n_js, codewars, task№20

--- src/tasks_from_Codewars/tasks.js
+++ src/tasks_from_Codewars/tasks.js
@@ -39,3 +39,34 @@ arr.map(item => {
   });
 });
 return arr.length == 0 ? 0 : count;
+
+
+
+
+// Write a function that returns the sum of two numbers. The input numbers are strings 
+// and the function must return a string.
+
+a = a.split("").reverse().join("");
+  b = b.split("").reverse().join("");
+  la = a.length;
+  lb = b.length;
+  var temp = [];
+  var bit = 0;
+  for(var i = 0; i < Math.max(la,lb); i++){
+    if(i>=la){
+      var cur = Number(b[i]);
+    }else if(i>=lb){
+      var cur =Number(a[i]);
+    }else{
+      var cur = Number(a[i]) + Number(b[i]);
+    }     
+    temp.push((cur+bit)%10);
+    bit = Math.floor((cur+bit)/10);
+  }
+  if(bit==1){
+    temp.push(1);
+  }  
+  var ans = "";
+  for(var i=temp.length-1; i>=0; i--)
+    ans+=temp[i];
+  return ans;
